29da4a3179a981f72027ce2ff960fa6fe19b1fe7
Naman <naman.upadhyay@mitwpu.edu.in>
2026-02-01 09:44:00 +0530

    Logging And Exception

---
+++ src/exception.py
@@ -0,0 +1,46 @@
+import sys
+# sys module provides various function and variables that are used to manipulate different parts of the Python runtime envirnoment
+from logger import logging 
+
+# The function is having the 2 parameters which is the error itself and the error_detail which will be present inside the sys
+def error_message_details(error,error_detail:sys):
+    _ , _ , exc_tb = error_detail.exc_info()
+    # It give (type , value , traceback) in return
+    # the exc_info() will give three values in which first 2 are not useful to use
+    # this var exc_tb will give that in which file the error has occured, in which line the error has occured and all this information will be stored inside this variable
+    
+    file_name = exc_tb.tb_frame.f_code.co_filename                    # This is the name of file in which the error has occured , All this code we can find from the exception handling documentation
+    
+    error_message = "Error occured in Python Script name [{0} line number [{1}] error message [{2}]".format(file_name , exc_tb.tb_lineno , str(error))
+    return error_message
+
+
+
+
+# Creating an Class which is inherited from the Exception Class which is inbuilt in Python
+class CustomException(Exception):
+    def __init__(self , error_message , error_detail:sys):
+        super().__init__(error_message)                                # The Super method is used to inherit the Exception Class over here
+        self.error_message = error_message_details(error_message , error_detail = error_detail)
+    
+    # Without this function the python doesn't know what to print , if we not write this it will print the location of the error message in the device , but with this function written it knows what to print
+    def __str__(self):
+        return self.error_message
+    # when we will print from the this custom_exception class the error_message will get printed
+    
+    
+    
+    # This is only for the one time , because it will be common for the rest of the projects
+    
+    
+    
+
+
+# For testing the Exception
+# if __name__ == '__main__':
+        
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.error("Unexpected Error Occured",exc_info = True)
+#         raise CustomException(e,sys)
---
+++ src/logger.py
@@ -0,0 +1,31 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Creating a Log file name , Which will use the current date time in the format which is given
+# for Example --> 02_01_2026_14_35_20 then after wards the .log is added as the extension
+# Every time you run the script → a unique log file is created.
+
+
+logs_path = os.path.join(os.getcwd(),"logs")
+# Whenever a log file will be created it will be created in the current working directory cwd with the prefix as logs
+# current_directory + "logs" + LOG_FILE
+# Example --> C:\Users\Naman\Project\logs\02_01_2026_14_35_20.log
+
+
+os.makedirs(logs_path,exist_ok=True)
+# It tell if there is logs_path file is there then append inside that file everytime when the logging is done
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+# For testing the logging
+# if __name__ == "__main__":
+#     logging.info("Logging Has Started")
